fb86c8998ce1506a92f76a09b74f99fa1637c1c1
Ivancast67 <labc202@uam.net.ni>
2024-08-26 10:54:15 -0600

    sesion3 26/8/2024

---
+++ Desktop/metodgrupo3/sesion3/sesion3/Program.cs
@@ -0,0 +1,23 @@
+﻿using sesion3.modelos;
+
+namespace sesion3
+{
+    internal class Program
+        // calcular la edad de una persona 
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            string nombre;
+            DateTime fechaNac;
+            Console.WriteLine("Nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Fecha de Nacimiento: ");
+            fechaNac = DateTime.Parse(Console.ReadLine());
+            persona.Nombre = nombre;
+            persona.FechaNac =fechaNac;
+            Console.WriteLine(persona.EvaluarEdad());
+
+        }
+    }
+}
